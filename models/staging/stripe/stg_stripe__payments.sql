bc81604f579292131439746641b86364e4d91afc
Fabrice Moncaut <fmoncaut@gmail.com>
2026-01-07 21:43:52 +0000

    creating file yamel stripe

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,8 +3,7 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    -- Le montant est en centimes, on le convertit en dollars/euros
     amount / 100 as amount,
     created as created_at
 
-from `dbt-tutorial.stripe.payment`
+from {{ source('stripe', 'payment') }}
