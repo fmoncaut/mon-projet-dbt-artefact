7b68a750c58122a8c004b37ed8e9f8cc20169f11
Fabrice Moncaut <fmoncaut@gmail.com>
2026-01-10 18:27:09 +0000

    added singular test to stg_stripe_payment

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,9 +1,25 @@
-select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
-    amount / 100 as amount,
-    created as created_at
+with source as (
 
-from {{ source('stripe', 'payment') }}
+    select * from {{ source('stripe', 'payment') }}
+
+),
+
+rename as (
+
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status as payment_status,
+        
+        -- Conversion des cents en dollars
+        amount / 100 as amount,
+        
+        -- ATTENTION : J'ai retiré la virgule ici car c'est la dernière colonne
+        created as created_at
+        
+    from source
+
+)
+
+select * from rename
